9b0ffbd0f0738d7b275ba8219936504d03fafa5b
mike shlapakov <mike.shlapakov@gmail.com>
2024-09-29 12:40:06 +0300

    Made code prettier

--- perlinNoise.js
+++ perlinNoise.js
@@ -23,20 +23,20 @@ class Color {
 }
 
 function pseudoRnd(x){
-    let f = Math.cos(Math.sin(x+1) * 1000)
-    return f - Math.floor(f);
+    let f = Math.cos(Math.sin(x) * 1000)
+    return f ;
 }
 
 function pseudoRndVec(vec2){
-    vec2 = [vec2[0] + 0.02, vec2[1] + 0.02];
-    x = dot(vec2, [123.4, 234.5]);
-    y = dot(vec2, [345.6, 456.7]);
-    let gradient = [x, y];
-    gradient = [Math.sin(gradient[0]),Math.cos(gradient[1])];
-    gradient = [gradient[0] * 1234.4321, gradient[1] * 5678.8765];
-    
-    gradient = [Math.cos(gradient[0]),Math.sin(gradient[1])];
-    return gradient;
+    vec2 = [vec2[0] + 1, vec2[1] + 1];
+    let x = dot(vec2, [123.4, 234.5]);
+    let y = dot(vec2, [345.6, 456.7]);
+
+    let grad = [Math.sin(x), Math.sin(y)];
+    grad = [grad[0] * 1234.4321, grad[1] * 5678.8765];
+    grad = [Math.cos(grad[0]),Math.sin(grad[1])];
+
+    return grad;
 }
 
 function lerp(a, b, t){
@@ -74,84 +74,96 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth * window.devicePixelRatio   || 1
 canvas.height = window.innerHeight * window.devicePixelRatio || 1
 
-const cellsize = 64   // grid size
-const cellLength = cellsize/2  //resulotion
-const gridLength = 16  // grid 
-const brightness = 2.0
+const noiseSize = 8 // noise size
+const cellsize = 2**noiseSize  
+const resolution = 2  //resolution
+const cellLength = cellsize/resolution; // ratio of the pixels
+const gridLength = 2**(9-noiseSize)  // grid size 
+const brightness = 0.9
 
 let perlin = new Array(gridLength).fill(new Array(gridLength).fill(new Array(cellLength).fill(new Array(cellLength).fill(null))));
 
 function cellEvaluation(x, y, vecArray){
-    let ratio = cellsize/cellLength;
     for (let i = 0; i < cellLength; i++) {
         for (let j = 0; j < cellLength; j++){
-            let center = [(j)*ratio+ratio/2, (i)*ratio+ratio/2]
+            let center = [(j)*resolution+resolution/2, (i)*resolution+resolution/2] // center of each pixel
             // console.log(center)
-            let distTl = [center[0]/cellsize-0, center[1]/cellsize-1];
-            let distTr = [center[0]/cellsize-1, center[1]/cellsize-1];
-            let distBl = [center[0]/cellsize-0, center[1]/cellsize-0];
-            let distBr = [center[0]/cellsize-1, center[1]/cellsize-0];
+            // distance of the pixel from the corners of the cell
+            let distTl = [center[0]/cellsize-0, center[1]/cellsize-0];
+            let distTr = [center[0]/cellsize-1, center[1]/cellsize-0];
+            let distBl = [center[0]/cellsize-0, center[1]/cellsize-1];
+            let distBr = [center[0]/cellsize-1, center[1]/cellsize-1];
 
-            let dotTl = dot(vecArray[0], distTl) // vecArray[0][0]*(center[0]/cellsize-0)+vecArray[0][1]*(0-center[1]/cellsize);
-            let dotTr = dot(vecArray[1], distTr) // vecArray[1][0]*(center[0]/cellsize-1)+vecArray[1][1]*(0-center[1]/cellsize);
-            let dotBl = dot(vecArray[2], distBl) // vecArray[2][0]*(center[0]/cellsize-0)+vecArray[2][1]*(1-center[1]/cellsize);
-            let dotBr = dot(vecArray[3], distBr) // vecArray[3][0]*(center[0]/cellsize-1)+vecArray[3][1]*(1-center[1]/cellsize);
-
+            // calculating the dot for each pixel
+            let dotTl = dot(vecArray.tl, distTl) // vecArray[0][0]*(center[0]/cellsize-0)+vecArray[0][1]*(0-center[1]/cellsize);
+            let dotTr = dot(vecArray.tr, distTr) // vecArray[1][0]*(center[0]/cellsize-1)+vecArray[1][1]*(0-center[1]/cellsize);
+            let dotBl = dot(vecArray.bl, distBl) // vecArray[2][0]*(center[0]/cellsize-0)+vecArray[2][1]*(1-center[1]/cellsize);
+            let dotBr = dot(vecArray.br, distBr) // vecArray[3][0]*(center[0]/cellsize-1)+vecArray[3][1]*(1-center[1]/cellsize);
+            
+            // cosine interpolation
             let TLTR = coserp(dotTl, dotTr, center[0]/cellsize)
             // console.log(dotTl, dotTr, center[0], TLTR)
             let BLBR = coserp(dotBl, dotBr, center[0]/cellsize)
             // console.log(dotBl, dotBr, center[1], BLBR)
-            let value = coserp(BLBR, TLTR, center[1]/cellsize)
+            let value = coserp(TLTR, BLBR, center[1]/cellsize)
             // console.log(value, x,y, j,i)
             
-            let color = Math.floor(255*quinticFade((value+0.3)))
-            ctx.fillStyle = new Color(color,color,color);
+            let color = Math.floor(255*quinticFade((value*brightness+0.3)))
+            // perlin[y][x][i][j]  = [color,color,color];
             // console.log(i, j, perlin[y][x][i][j].toString())
-            ctx.fillRect(j*ratio+x*cellsize,i*ratio+y*cellsize, ratio, ratio);
+            ctx.fillStyle = new Color(color,color,color);
+            ctx.fillRect(j*resolution+x*cellsize,i*resolution+y*cellsize, resolution, resolution);
         }
     }
 }
 
 function matrixToPNG(matrix, pixelSize = 1) {
-    // Draw the matrix on the canvas
+    let renderTime = performance.now();
+    // calculate for the grid
     for (let y = 0; y < gridLength; y++) {
         for (let x = 0; x < gridLength; x++) {
 
-            let tr = [(x+1), ( y+1 )] 
-            let tl = [( x ), ( y+1 )] 
-            let br = [(x+1), ( y )]
-            let bl = [( x ), ( y )]
+            // calculate for the points of the cell
+            let tl = [ x ,  y ]
+            let tr = [x+1,  y ]
+            let bl = [ x , y+1] 
+            let br = [x+1, y+1] 
             
-            // console.log(tl, tr, bl, br);
-            ctx.strokeStyle = '#00FF00';
-            // console.log(tl[0]*gridLength, tl[1]*gridLength, br[0]*gridLength, br[1]*gridLength)
-            // ctx.strokeRect(tl[0]*cellLength, tl[1]*cellLength, cellsize, cellsize);
+            // console.log(bl, br, tl, tr);
+            // ctx.sbrokeStyle = '#00FF00';
+            // console.log(bl[0]*gridLength, bl[1]*gridLength, tr[0]*gridLength, tr[1]*gridLength)
+            // ctx.sbrokeRect(bl[0]*cellLength, bl[1]*cellLength, cellsize, cellsize);
 
-            ctx.strokeStyle = '#ff0000';
-            let trVec = pseudoRndVec(tr) //[Math.cos(pseudoRnd(3*tr[0]+tr[1]*cellLength)*2*Math.PI), Math.sin(pseudoRnd(3*tr[0]+tr[1]*cellLength)*2*Math.PI)]// rotateVector([ 1, 1], 1) * 2*Math.PI)
-            ctx.beginPath();
-            ctx.moveTo(tr[0]*cellsize, tr[1]*cellsize);
-            ctx.lineTo(tr[0]*cellsize+trVec[0]*cellsize, tr[1]*cellsize+trVec[1]*cellsize);
+            // // transform each point to a random vector
+
+            // ctx.sbrokeStyle = '#FF0000';
+            let tlVec = pseudoRndVec(tl) //[Math.cos(pseudoRnd(3*tl[0]+tl[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*tl[0]+tl[1]*cellsize)*2*Math.PI)]// rotateVector([-1,-1], *gridLength+x) * 2*Math.PI) 
+            // ctx.beginPath();
+            // ctx.moveTo(tl[0]*cellsize, tl[1]*cellsize);
+            // ctx.lineTo(tl[0]*cellsize+tlVec[0]*cellsize, tl[1]*cellsize+tlVec[1]*cellsize);
             // ctx.stroke();
-            let tlVec = pseudoRndVec(tl) //[Math.cos(pseudoRnd(3*tl[0]+tl[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*tl[0]+tl[1]*cellsize)*2*Math.PI)]// rotateVector([-1, 1],  * 2*Math.PI) 
-            ctx.beginPath();
-            ctx.moveTo(tl[0]*cellsize, tl[1]*cellsize);
-            ctx.lineTo(tl[0]*cellsize+tlVec[0]*cellsize, tl[1]*cellsize+tlVec[1]*cellsize);
+            let trVec = pseudoRndVec(tr) //[Math.cos(pseudoRnd(3*tr[0]+tr[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*tr[0]+tr[1]*cellsize)*2*Math.PI)]// rotateVector([ 1,-1], *gridLength+x+1) * 2*Math.PI) 
+            // ctx.beginPath();
+            // ctx.moveTo(tr[0]*cellsize, tr[1]*cellsize);
+            // ctx.lineTo(tr[0]*cellsize+trVec[0]*cellsize, tr[1]*cellsize+trVec[1]*cellsize);
             // ctx.stroke();
-            let brVec = pseudoRndVec(br) //[Math.cos(pseudoRnd(3*br[0]+br[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*br[0]+br[1]*cellsize)*2*Math.PI)]// rotateVector([ 1,-1], *gridLength+x+1) * 2*Math.PI) 
-            ctx.beginPath();
-            ctx.moveTo(br[0]*cellsize, br[1]*cellsize);
-            ctx.lineTo(br[0]*cellsize+brVec[0]*cellsize, br[1]*cellsize+brVec[1]*cellsize);
+            let blVec = pseudoRndVec(bl) //[Math.cos(pseudoRnd(3*bl[0]+bl[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*bl[0]+bl[1]*cellsize)*2*Math.PI)]// rotateVector([-1, 1],  * 2*Math.PI) 
+            // ctx.beginPath();
+            // ctx.moveTo(bl[0]*cellsize, bl[1]*cellsize);
+            // ctx.lineTo(bl[0]*cellsize+blVec[0]*cellsize, bl[1]*cellsize+blVec[1]*cellsize);
             // ctx.stroke();
-            let blVec = pseudoRndVec(bl) //[Math.cos(pseudoRnd(3*bl[0]+bl[1]*cellsize)*2*Math.PI), Math.sin(pseudoRnd(3*bl[0]+bl[1]*cellsize)*2*Math.PI)]// rotateVector([-1,-1], *gridLength+x) * 2*Math.PI) 
-            ctx.beginPath();
-            ctx.moveTo(bl[0]*cellsize, bl[1]*cellsize);
-            ctx.lineTo(bl[0]*cellsize+blVec[0]*cellsize, bl[1]*cellsize+blVec[1]*cellsize);
+            let brVec = pseudoRndVec(br) //[Math.cos(pseudoRnd(3*br[0]+br[1]*cellLength)*2*Math.PI), Math.sin(pseudoRnd(3*br[0]+br[1]*cellLength)*2*Math.PI)]// rotateVector([ 1, 1], 1) * 2*Math.PI)
+            // ctx.beginPath();
+            // ctx.moveTo(br[0]*cellsize, br[1]*cellsize);
+            // ctx.lineTo(br[0]*cellsize+brVec[0]*cellsize, br[1]*cellsize+brVec[1]*cellsize);
             // ctx.stroke();
 
-            cellEvaluation(x,y, [tlVec,trVec,blVec,brVec]) 
+            // calculate the gradient for the cell
+            cellEvaluation(x,y, {bl:blVec, br:brVec, tl:tlVec, tr:trVec}) 
         }
     }
+    console.log(Math.floor(performance.now() - renderTime));
+    // console.log(perlin)
     // // Convert canvas to PNG data URL
     // const dataURL = canvas.toDataURL('image/png');
     
